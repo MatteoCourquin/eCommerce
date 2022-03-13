ff616951d115fa1fa05333908ae1e17215b9990b
« Matteo <matteo.courquin@gmail.com>
2022-03-13 12:22:49 +0100

    :construction: SAV project

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProductsCrudController extends AbstractCrudController
@@ -12,14 +16,12 @@ class ProductsCrudController extends AbstractCrudController
         return Products::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextEditorField::new('description')
         ];
     }
-    */
 }
